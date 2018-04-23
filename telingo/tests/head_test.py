62022b3906d8c280e107d61a7ea4f786860ff960
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2018-04-23 21:28:11 +0200

    tests for interval set

--- telingo/tests/head_test.py
+++ telingo/tests/head_test.py
@@ -75,6 +75,19 @@ class TestHead(TestCase):
         self.assertEqual(formula_to_theory_atom(">a"), "&tel(__t) { >(a) :  }")
         """
 
+    def test_interval_set(self):
+        self.assertEqual(str(th.IntervalSet([(1,1)])), "{}")
+        self.assertEqual(str(th.IntervalSet([(1,2)])), "{[1,2)}")
+        self.assertEqual(str(th.IntervalSet([(1,2), (3,4)])), "{[1,2),[3,4)}")
+        self.assertEqual(str(th.IntervalSet([(1,2), (3,4), (-1,0)])), "{[-1,0),[1,2),[3,4)}")
+        self.assertEqual(str(th.IntervalSet([(1,2),(3,4),(2,3)])), "{[1,4)}")
+        self.assertEqual(str(th.IntervalSet([(1,2),(3,4),(5,7),(0,10)])), "{[0,10)}")
+        self.assertIn((0,0), th.IntervalSet([(1,2),(3,4)]))
+        self.assertIn((1,2), th.IntervalSet([(1,2),(3,4)]))
+        self.assertIn((3,4), th.IntervalSet([(1,2),(3,5)]))
+        self.assertIn((4,5), th.IntervalSet([(1,2),(3,5)]))
+        self.assertNotIn((1,4), th.IntervalSet([(1,2),(3,4)]))
+
     def test_variables(self):
         self.assertEqual(str(th.get_variables(parse_atom("p(X,Y) | a(X,Z)"))), "[X, Y, Z]")
 
