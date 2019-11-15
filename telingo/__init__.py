0b7842b9f4a9c03e972ff5e0339bf8636ca55f60
FrancoisLaferriere <flaferriere@brehat.haiti.cs.uni-potsdam.de>
2019-11-15 16:02:51 +0100

    modify version

--- telingo/__init__.py
+++ telingo/__init__.py
@@ -86,7 +86,7 @@ class Application:
         See clingo.clingo_main().
         """
         self.program_name = "telingo"
-        self.version = "1.0.0"
+        self.version = "2.0.1"
 
         self.__imin = 0
         self.__imax = None
