c19b349e3294bb4e2a8a4b6eaabd55a597ded0e0
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2022-05-18 00:17:51 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from textwrap import dedent
 
 setup(
     version = '2.1.3',
     name = 'telingo',
     description = 'System to solve dynamic temporal logic programs.',
+    long_description = dedent('''\
+        Telingo is a solver for temporal programs. It leaverages clingo's input
+        language and scripting cababilities to parse and solve programs with
+        temporal formulas. As such the input of telingo is valid clingo input
+        supporting all clingo language features like for example aggregates;
+        only the way programs are grounded and solved is adjusted.
+        '''),
+    long_description_content_type='text/markdown',
     author = 'Roland Kaminski & Francois Laferriere',
     author_email='kaminski@cs.uni-potsdam.de',
     url='https://github.com/potassco/telingo',
@@ -20,4 +29,3 @@ setup(
         ]
     }
 )
-
